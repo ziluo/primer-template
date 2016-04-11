1d5b56234b720b5b968f5d7f2ebc079bf4aa1c94
leungwensen <leungwensen@gmail.com>
2016-04-11 11:12:22 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -4,6 +4,17 @@ console.log(compile('I am <%=name%>, and i like playing <%=man.game%>.'));
 
 console.log(compile('I am <%=name%><%if(man){%> and i like playing <%=man.game%><%}%>.'));
 
+console.log(compile([
+    'I am <%=name%><%if(man){%> and I like playing <%=man.game%><%}%>.',
+    'My favorite animates are ',
+    '<% animates.each(function(animate){ %>',
+        '<% if (animate.type !== invisibleType ){ %>',
+            '<%=animate.name%>, ',
+        '<% } %>',
+    '<% }); %>',
+    'etc.',
+].join('\n')));
+
 //console.log(compile('I am <%=name%><%if(man){%> and i like playing <%=man.game%><%}%>.<%include("./include.atpl")%>', ''));
 
 
